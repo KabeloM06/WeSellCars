aadfca986f3a197f0f749bcf61bf07652345e251
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-06-07 11:43:10 +0200

    Logging API requests

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,35 @@
 const express =require('express');
 
 const app = express();
+const morgan = require('morgan');
 
-app.get('/', (req, res) =>{
-    res.send('Hello Kabelo');
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+//Middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.get(`${api}/products`, (req, res) =>{
+    const product ={
+        id: 1,
+        name: 'hair Dresser',
+        image: 'some_url',
+    }
+    
+    res.send(product);
+}
+)
+app.post(`${api}/products`, (req, res) =>{
+    const newProduct = req.body;
+    console.log(newProduct);
+    
+    res.send(product);
 }
 )
 
 app.listen(3000, ()=> {
+    console.log(api);
     console.log('server is running on http://localhost:3000')
 })
